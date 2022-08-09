53a0e1f5e27dc7766426391793e8be421304c8bc
Williams Momoh <williamsmomoh@gmail.com>
2022-08-09 10:10:37 -0700

    Corrected the error for print string function

--- print_string.c
+++ print_string.c
@@ -7,9 +7,9 @@
  */
 int print_string(char *string)
 {
-	int count = 0;
+	int i, count = 0;
 
-	for (int i = 0; string[i] != '\0'; i++)
+	for (i = 0; string[i] != '\0'; i++)
 	{
 		count += _putchar(string[i]);
 	}
