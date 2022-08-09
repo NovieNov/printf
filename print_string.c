11757e1eec8ca85651cc53c56e354ac4f49e635c
Williams Momoh <williamsmomoh@gmail.com>
2022-08-09 07:31:11 -0700

    print string function added

---
+++ print_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_string - function to print string
+ * @string: string to be printed
+ * Return: Count, number of characters in string
+ */
+int print_string(char *string)
+{
+	int count = 0;
+
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		count += _putchar(string[i]);
+	}
+	return (count);
+}
